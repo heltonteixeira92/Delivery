60de7cb08a196cefdbcb92bf7e2141fcb540204b
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-17 01:54:05 -0300

    adicionado cupom de desconto e finalizacao de pedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import ItemPedido, Pedido
+from .models import ItemPedido, Pedido, CupomDesconto
 from django.http import HttpResponse # noqa
+
+
+@admin.register(CupomDesconto)
+class CupomDescontoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'desconto', 'ativo')
+    readonly_fields = ('usos',)
 
 
 class itemPedidoInline(admin.TabularInline):
